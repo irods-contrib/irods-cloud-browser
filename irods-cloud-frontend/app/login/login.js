52953be118174396d9c7bead01d88b46ab2d651c
Cesar Garde <cesargarde@dhcp152-54-6-164.wireless.europa.renci.org>
2015-03-27 16:37:03 -0400

    adding login CG

---
+++ irods-cloud-frontend/app/login/login.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('myApp.login', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {
+    templateUrl: 'login/login.html',
+    controller: 'loginCtrl'
+  });
+}])
+
+.controller('loginCtrl', [function() {
+
+}]);

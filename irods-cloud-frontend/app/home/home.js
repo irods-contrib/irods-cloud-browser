376d3d24629288835bad8e2d4065c98d33530cb5
mconway <michael.c.conway@gmail.com>
2015-04-07 15:22:04 -0400

    #1 first test of home

--- irods-cloud-frontend/app/home/home.js
+++ irods-cloud-frontend/app/home/home.js
@@ -9,8 +9,26 @@ angular.module('myApp.home', ['ngRoute'])
   });
 }])
 
-.controller('homeCtrl', [function() {
+    .controller('homeCtrl', ['$scope','$log', '$http', '$location', 'MessageService',function ($scope, $log, $http, $location, MessageService) {
 
+        /**
+         * List all virtual collections for the user
+         */
+        $scope.listVirtualCollections = function () {
 
+            $log.info("getting virtual colls");
+            return $http({method: 'GET', url: 'http://localhost:8080/irods-cloud-backend/virtualCollection'}).success(function (data) {
+                $scope.virtualCollections = data;
+            }).error(function () {
+                $scope.virtualCollections = [];
+            });
+
+        };
+
+        /**
+         * INIT
+         */
+
+        $scope.listVirtualCollections();
 
 }]);

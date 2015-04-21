7e69a3e6e47751f7ffb46dbddc36cab2cbd2ec36
Cesar Garde <cesargarde@dhcp152-54-6-164.wireless.europa.renci.org>
2015-04-21 15:07:29 -0400

    profile page connected

--- irods-cloud-frontend/app/profile/profile.js
+++ irods-cloud-frontend/app/profile/profile.js
@@ -20,7 +20,47 @@ angular.module('myApp.profile', ['ngRoute'])
     .controller('profileCtrl', ['$scope','$log', '$http', '$location', 'MessageService','globals','breadcrumbsService','virtualCollectionsService','collectionsService','fileService','dataProfile',function ($scope, $log, $http, $location, MessageService, $globals, breadcrumbsService, $virtualCollectionsService, $collectionsService, fileService, dataProfile) {
 
        $scope.dataProfile = dataProfile;
+        /*
+         Get a default list of the virtual collections that apply to the logged in user, for side nav
+         */
+        
+        $scope.listVirtualCollections = function () {
 
+            $log.info("getting virtual colls");
+
+            return $http({method: 'GET', url: $globals.backendUrl('virtualCollection')}).success(function (data) {
+                $scope.virtualCollections = data;
+            }).error(function () {
+                $scope.virtualCollections = [];
+            });
+        };
+       $scope.listVirtualCollections();
+       var side_nav_toggled = "no";
+        $scope.side_nav_toggle = function () {
+            if (side_nav_toggled == "no") {
+                side_nav_toggled = "yes";
+                $('.side_nav_options').animate({'opacity': '0'});
+                $('#side_nav').animate({'width': '3%'});
+                $('#main_contents').animate({'width': '96.9%'});
+                $('.side_nav_toggle_button').text('>>');
+            } else if (side_nav_toggled == "yes") {
+                side_nav_toggled = "no";
+                $('#main_contents').animate({'width': '81.9%'});
+                $('#side_nav').animate({'width': '18%'});
+                $('.side_nav_options').animate({'opacity': '1'});
+                $('.side_nav_toggle_button').text('<<');
+            }
+        };
+        $scope.green_action_toggle= function($event){
+          var content = $event.currentTarget.parentElement.nextElementSibling;
+          var container = $event.currentTarget.parentElement.parentElement;
+          $(content).toggle('normal');
+          $(container).toggleClass('green_toggle_container_open');
+        };
+
+        $scope.back_button = function (){
+          history.go(-1);
+        }
       
 }]);
    

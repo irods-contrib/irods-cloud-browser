71e84e18d16394cb1bb79db95484ec790682056b
michael-conway <michael_conway@unc.edu>
2015-07-21 09:09:31 -0400

    #2 css now in dist

--- irods-cloud-frontend/gulpfile.js
+++ irods-cloud-frontend/gulpfile.js
@@ -7,19 +7,19 @@ var concat = require('gulp-concat');
 var del = require('del');
 
 
-gulp.task('default', function() {
+gulp.task('default', function () {
     // place code for your default task here
 });
 
 /**
  * Create a dist subdirectory that has the assembled javascript, css, images, html assets
  */
-gulp.task('dist', function() {
-    gulp.start('clean','vendor-scripts', 'app-scripts');
+gulp.task('dist', function () {
+    gulp.start('clean', 'vendor-scripts', 'app-scripts', 'css');
 
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
     del(['./dist'], function (err, paths) {
         console.log('Deleted files/folders:\n', paths.join('\n'));
     });
@@ -28,14 +28,16 @@ gulp.task('clean', function() {
 /**
  * assemble all vendor javascripts into a 'vendor.js'
  */
-gulp.task('vendor-scripts', function() {
+gulp.task('vendor-scripts', function () {
     return gulp.src(['./bower_components/angular/angular.min.js', './bower_components/jquery/dist/jquery.min.js',
-    './bower_components/angular-route/angular-route.js', './bower_components/masonry/dist/masonry.pkgd.min.js',
-    './bower_components/jquery-ui/jquery-ui.min.js','./bower_components/bootstrap/dist/js/bootstrap.min.js',
-    './bower_components/angular-animate/angular-animate.min.js','./bower_components/angular-message-center/dist/message-center.min.js',
-    './bower_components/angular-message-center/message-center-templates.jss',
-    './bower_components/ng-file-upload/ng-file-upload-shim.min.js',
-    './bower_components/ng-file-upload/ng-fil-upload.min.js'])
+        './bower_components/angular-route/angular-route.js', './bower_components/masonry/dist/masonry.pkgd.min.js',
+        './bower_components/jquery-ui/jquery-ui.min.js',
+        './bower_components/jquery-mobile/jquery.mobile.js',
+        './bower_components/bootstrap/dist/js/bootstrap.min.js',
+        './bower_components/angular-animate/angular-animate.min.js', './bower_components/angular-message-center/dist/message-center.min.js',
+        './bower_components/angular-message-center/message-center-templates.jss',
+        './bower_components/ng-file-upload/ng-file-upload-shim.min.js',
+        './bower_components/ng-file-upload/ng-fil-upload.min.js'])
         .pipe(concat('vendor.js'))
         .pipe(gulp.dest('./dist/js'));
 });
@@ -43,10 +45,10 @@ gulp.task('vendor-scripts', function() {
 /**
  * assemble all application code javascripts into an 'app.js'
  */
-gulp.task('app-scripts', function() {
+gulp.task('app-scripts', function () {
     return gulp.src(['./app/components/*.js',
-    './app/home/*.js', './app/login/*.js', './app/metadata/*.js',
-    './app/profile/*.js'])
+        './app/home/*.js', './app/login/*.js', './app/metadata/*.js',
+        './app/profile/*.js'])
         .pipe(concat('app.js'))
         .pipe(gulp.dest('./dist/js'));
 });
@@ -55,13 +57,15 @@ gulp.task('app-scripts', function() {
  * assemble and minify all css assets
  */
 
-gulp.task('css', function() {
-    gulp.task('app-css', function() {
-        return gulp.src(['./app/components/*.js',
-            './app/home/*.js', './app/login/*.js', './app/metadata/*.js',
-            './app/profile/*.js', './app/gallery/*.js'])
-            .pipe(concat('app.js'))
-            .pipe(gulp.dest('./dist/js'));
-    });
+gulp.task('css', function () {
+        return gulp.src(['./app/app.css',
+            './bower_components/html5-boilerplate/css/*.css',
+            './bower_components/angular-message-center/message-center.css',
+            './bower_components/bootstrap/dist/css/bootstrap.min.css',
+            './app/sb-admin.css',
+            './app/css/main.css'
+        ])
+            .pipe(concat('app.css'))
+            .pipe(gulp.dest('./dist/css'));
 
 });

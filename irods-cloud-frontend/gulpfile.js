61895780785609496a5b158a421b3b5f89643f1a
michael-conway <michael_conway@unc.edu>
2015-07-20 22:05:25 -0400

    fixed author

--- irods-cloud-frontend/gulpfile.js
+++ irods-cloud-frontend/gulpfile.js
@@ -13,7 +13,17 @@ gulp.task('default', function() {
 gulp.task('vendor-scripts', function() {
     return gulp.src(['./bower_components/angular/angular.min.js', './bower_components/jquery/dist/jquery.min.js',
     './bower_components/angular-route/angular-route.js', './bower_components/masonry/dist/masonry.pkgd.min.js',
-    './bower_components/jquery-ui/jquery-ui.min.js','./bower_components/bootstrap/dist/js/bootstrap.min.js'])
+    './bower_components/jquery-ui/jquery-ui.min.js','./bower_components/bootstrap/dist/js/bootstrap.min.js',
+    './bower_components/angular-animate/angular-animate.min.js','./bower_components/angular-message-center/dist/message-center.min.js',
+    './bower_components/angular-message-center/message-center-templates.jss',
+    './bower_components/ng-file-upload/ng-file-upload-shim.min.js',
+    './bower_components/ng-file-upload/ng-fil-upload.min.js'])
         .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('app-scripts', function() {
+    return gulp.src(['./app/components/*.js'])
+        .pipe(concat('app.js'))
         .pipe(gulp.dest('./dist/js'));
 });

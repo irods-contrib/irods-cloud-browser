fd2fd67e2d78f5511dd438aa5af21a07f35bf6e3
michael-conway <michael_conway@unc.edu>
2015-08-27 08:18:49 -0400

    #79 fixed

--- irods-cloud-frontend/app/login/login.js
+++ irods-cloud-frontend/app/login/login.js
@@ -31,6 +31,7 @@ angular.module('myApp.login', ['ngRoute'])
         };
 
         $scope.current_page = 'login';
+        $scope.login.authType="STANDARD";
         $scope.submitLogin = function () {
             var actval = irodsAccount($scope.login.host, $scope.login.port, $scope.login.zone, $scope.login.userName, $scope.login.password, $scope.login.authType, "");
             

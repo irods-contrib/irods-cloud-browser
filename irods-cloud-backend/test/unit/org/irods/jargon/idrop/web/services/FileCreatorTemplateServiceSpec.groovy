e747dc1647b81f01bdea08a7923398ea291a0da5
michael-conway <michael_conway@unc.edu>
2015-12-16 09:54:37 -0500

    #130 added fileservice test for create

--- irods-cloud-backend/test/unit/org/irods/jargon/idrop/web/services/FileCreatorTemplateServiceSpec.groovy
+++ irods-cloud-backend/test/unit/org/irods/jargon/idrop/web/services/FileCreatorTemplateServiceSpec.groovy
@@ -1,11 +1,12 @@
 package org.irods.jargon.idrop.web.services
 
-import grails.test.mixin.TestFor
+import grails.test.mixin.*
 
 import org.irods.jargon.core.connection.IRODSAccount
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory
 import org.irods.jargon.filetemplate.FileTemplate
 import org.irods.jargon.filetemplate.FileTemplateService
+import org.irods.jargon.filetemplate.TemplateCreatedFile
 
 import spock.lang.Specification
 
@@ -39,6 +40,34 @@ class FileCreatorTemplateServiceSpec extends Specification {
 		when:
 
 		def actual = fileCreatorTemplateService.listAlltemplates(irodsAccount)
+
+		then:
+
+		actual != null
+	}
+
+	void "should create a file based on a template"() {
+		given:
+		IRODSAccount irodsAccount = IRODSAccount.instance("host", 1247, "user", "password", "", "zone", "")
+		String absPath = "/a/b/c.txt"
+		def irodsAccessObjectFactory = mockFor(IRODSAccessObjectFactory)
+		def irodsAccessObjectFactoryMock = irodsAccessObjectFactory.createMock()
+
+		def fileTemplateService = mockFor(FileTemplateService)
+		def templateCreatedFile = new TemplateCreatedFile()
+		fileTemplateService.demand.createFileBasedOnTemplateUniqueIdentifier{parPath,name, type -> return templateCreatedFile}
+		def fileTemplateServiceMock = fileTemplateService.createMock()
+
+		def jargonServiceFactoryService = mockFor(JargonServiceFactoryService)
+		jargonServiceFactoryService.demand.instanceFileTemplateService{act1 -> return fileTemplateServiceMock}
+		def jargonServiceFactoryServiceMock = jargonServiceFactoryService.createMock()
+
+		def fileCreatorTemplateService = new FileCreatorTemplateService()
+		fileCreatorTemplateService.irodsAccessObjectFactory = irodsAccessObjectFactoryMock
+		fileCreatorTemplateService.jargonServiceFactoryService = jargonServiceFactoryServiceMock
+		when:
+
+		def actual = fileCreatorTemplateService.createFileByTemplate(irodsAccount, "x", "y", "z")
 
 		then:
 

2b7922164a052b2556e3192beb0fe3b6f9e79caf
michael-conway <michael_conway@unc.edu>
2015-12-18 11:53:48 -0500

    #133 rule composition workbench service

--- irods-cloud-backend/grails-app/services/org/irods/jargon/idrop/web/services/RuleWorkbenchService.groovy
+++ irods-cloud-backend/grails-app/services/org/irods/jargon/idrop/web/services/RuleWorkbenchService.groovy
@@ -4,6 +4,7 @@ import org.irods.jargon.core.connection.IRODSAccount
 import org.irods.jargon.core.pub.IRODSAccessObjectFactory
 
 
+
 /**
  * Service to manage rule editing via the rule workbench and management of the delay exec queue
  * @author Mike Conway - DICE
@@ -24,6 +25,11 @@ class RuleWorkbenchService {
 	 */
 	def loadRuleFromIrods(String irodsAbsolutePath, IRODSAccount irodsAccount) {
 		log.info("loadRuleFromIrods()")
+
+		if (!irodsAccount) {
+			throw new IllegalArgumentException("null irodsAccount")
+		}
+
 		if (!irodsAbsolutePath) {
 			throw new IllegalArgumentException("null or empty irodsAbsolutePath")
 		}
@@ -31,5 +37,47 @@ class RuleWorkbenchService {
 
 		def ruleCompositionService = jargonServiceFactoryService.instanceRuleCompositionService(irodsAccount)
 		return ruleCompositionService.loadRuleFromIrods(irodsAbsolutePath)
+	}
+
+	/**
+	 * Store a rule from its constituent parts
+	 * @param ruleAbsolutePath
+	 * @param ruleBody
+	 * @param inputParameters
+	 * @param outputParameters
+	 * @param irodsAccount
+	 * @return
+	 */
+	def storeRuleFromParts( String ruleAbsolutePath,
+			String ruleBody,   List inputParameters,
+			List outputParameters,  IRODSAccount irodsAccount) {
+
+		log.info("storeRuleFromParts()")
+		if (ruleAbsolutePath == null || ruleAbsolutePath.isEmpty()) {
+			throw new IllegalArgumentException("null or empty ruleAbsolutePath")
+		}
+
+		if (ruleBody == null || ruleBody.isEmpty()) {
+			throw new IllegalArgumentException("null or empty ruleBody")
+		}
+
+		if (inputParameters == null) {
+			throw new IllegalArgumentException("null inputParameters")
+		}
+
+		if (outputParameters == null) {
+			throw new IllegalArgumentException("null outputParameters")
+		}
+
+		if (!irodsAccount) {
+			throw new IllegalArgumentException("null irodsAccount")
+		}
+
+		log.info("ruleAbsolutePath:${ruleAbsolutePath}")
+		log.info("inputParameters:${inputParameters}")
+		log.info("outputParameters:${outputParameters}")
+
+		def ruleCompositionService = jargonServiceFactoryService.instanceRuleCompositionService(irodsAccount)
+		return ruleCompositionService.storeRuleFromParts(ruleAbsolutePath, ruleBody, inputParameters, outputParameters)
 	}
 }

8a516d907cc260dc8a8724cdcb06c5b1dd87ecba
michael-conway <michael_conway@unc.edu>
2016-05-19 11:13:14 -0400

    #161 remove node and bower deps

--- irods-cloud-frontend/bower_components/jquery-mobile/js/jquery.mobile.core.js
+++
@@ -1,9 +0,0 @@
-//>>excludeStart("jqmBuildExclude", pragmas.jqmBuildExclude);
-//>>group: exclude
-
-define([
-	"./jquery.mobile.defaults",
-	"./jquery.mobile.data",
-	"./jquery.mobile.helpers"
-], function() {});
-//>>excludeEnd("jqmBuildExclude");

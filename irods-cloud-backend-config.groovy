af920276d854f1d853b6e51b58e117d273274db2
michael-conway <michael_conway@unc.edu>
2017-01-24 14:12:35 -0500

    #1 update sample config for negotiation

--- irods-cloud-backend-config.groovy
+++ irods-cloud-backend-config.groovy
@@ -10,3 +10,5 @@ beconf.login.preset.port=1247
 beconf.login.preset.zone='tempZone'
 beconf.login.preset.auth.type='STANDARD'
 beconf.login.preset.enabled=true
+// SSL negotiation policy for secure iRODS connections
+beconf.negotiation.policy='CS_NEG_DONT_CARE' // NO_NEGOTIATION, CS_NEG_REFUSE, CS_NEG_REQUIRE, CS_NEG_DONT_CARE
